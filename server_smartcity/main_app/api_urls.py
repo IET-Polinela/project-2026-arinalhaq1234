ec65db2645b1e9d86cea7bd3dfe4d00d7118db78
m arinal haq <arilhaq700@gmail.com>
2026-06-10 14:52:09 +0700

    prepare deployment lab session 13

---
+++ server_smartcity/main_app/api_urls.py
@@ -0,0 +1,18 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .api_views import ReportViewSet
+
+
+router = DefaultRouter()
+
+router.register(
+    r"report",
+    ReportViewSet,
+    basename="report",
+)
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
---
+++ server_smartcity/smartcity_app/urls.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from django.urls import include, path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from usermanagement_24782052.api_views import RegisterView
+from usermanagement_24782052 import views as user_views
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+
+    # Halaman Django biasa
+    path("", include("main_app.urls")),
+    path("about/", include("about.urls")),
+    path("contacts/", include("contacts.urls")),
+    path("dashboard/", include("dashboard_24782052.urls")),
+
+    # Endpoint REST API
+    path("api/", include("main_app.api_urls")),
+
+    # Autentikasi API JWT
+    path("api/register/", RegisterView.as_view(), name="api_register"),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path(
+        "api/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
+    ),
+
+    # Autentikasi halaman Django biasa
+    path("login/", user_views.CustomLoginView.as_view(), name="login"),
+    path("logout/", user_views.CustomLogoutView.as_view(), name="logout"),
+    path("register/", user_views.register, name="register"),
+]
